3982c19925ead10002183cb9f4be3b8a4bb8b8d6
yellowOctopus <greencloud11@naver.com>
2022-01-25 19:09:08 +0900

    success

---
+++ 20220125.js
@@ -0,0 +1,15 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  const n = data.split(' ');
+  const a = Number(n[0]),
+    b = Number(n[1]);
+
+  for (let i = b; i--; ) {
+    console.log('*'.repeat(a)); // console.log('*' * a)를 하면 NaN이 뜸
+    // repeat() 메소드는 문자열을 주어진 횟수만큼 반복해 붙인 새로운 문자열 반환
+    // str.repeat(count); -> 매개변수 count는 문자열을 반복할 횟수
+  }
+});
+
+// 다른 사람 풀이
+console.log(('*'.repeat(a) + '\n').repeat(b));
