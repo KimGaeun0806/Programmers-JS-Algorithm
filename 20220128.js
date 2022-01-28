59ca9129d7736b0b92a9ed0ba2db130968c29c2d
yellowOctopus <greencloud11@hanyang.ac.kr>
2022-01-28 13:07:56 +0900

    success

--- 20220128.js
+++ 20220128.js
@@ -13,7 +13,7 @@ return phone_number.replace(/\d(?=\d{4})/g, '*');
 // replace() 메소드는 어떤 패턴에 일치하는 일부 또는 모든 부분이 교체된 새로운 문자열을 반환함
 // str.replace(regexp|substr, newSjbstr|function) -> str.replace(pattern, replacement) 형식
 
-// \d     -> 숫자를 의미. [0-9]와 동일
-// x{n}   -> x 문자가 n번 반복됨을 의미
-// g      -> 대상 문자열 내 모든 패턴들을 검색하는 것을 의미
+// \d -> 숫자를 의미. [0-9]와 동일
+// x{n} -> x 문자가 n번 반복됨을 의미
+// g -> 대상 문자열 내 모든 패턴들을 검색하는 것을 의미
 // x(?=y) -> y가 뒤따라오는 x에만 대응됨
