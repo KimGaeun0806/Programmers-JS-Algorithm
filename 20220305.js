112cc8c8c19a943f7680109d5f3678d89442f2ee
yellowOctopus <greencloud11@hanyang.ac.kr>
2022-03-07 11:31:21 +0900

    success

--- 20220305.js
+++ 20220305.js
@@ -12,6 +12,7 @@ function solution(N, stages) {
     return stages.filter((e) => e === a).length / under[i];
   });
   return arr.sort((a, b) => fail[b - 1] - fail[a - 1]);
+  // arr.filter((e) => e === a).length로 배열 속 a의 개수 구하기
 }
 
 // 다른 사람 풀이
@@ -19,7 +20,9 @@ function solution2(N, stages) {
   let result = [];
   for (let i = 1; i <= N; i++) {
     let reach = stages.filter((x) => x >= i).length;
+    // 배열에서 i 이상인 값 개수 구하기
     let curr = stages.filter((x) => x === i).length;
+    // 배열에서 i인 값 개수 구하기
     result.push([i, curr / reach]);
   }
   result.sort((a, b) => b[1] - a[1]);
