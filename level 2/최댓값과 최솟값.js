0ca88d28c40b994506a2e51ea0e7e844ed158c75
KimGaeun0806 <greencloud11@hanyang.ac.kr>
2022-05-27 02:53:17 +0900

    success

---
+++ level 2/최댓값과 최솟값.js
@@ -0,0 +1,12 @@
+// LV2_연습문제: 최댓값과 최솟값 - 20220527
+
+function solution(s) {
+  let newS = s.split(' ').map((a) => a * 1);
+  return `${Math.min(...newS)} ${Math.max(...newS)}`;
+}
+
+// 다른 사람 풀이
+function solution2(s) {
+  const arr = s.split(' ');
+  return Math.min(...arr) + ' ' + Math.max(...arr);
+}
