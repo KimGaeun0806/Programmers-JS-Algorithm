68f301f8d9903d2c1a1f42e3e3b964df8f0a5f43
KimGaeun <greencloud11@hanyang.ac.kr>
2023-01-27 13:07:31 +0900

    solved 20230127

--- level 2/JadenCase 문자열 만들기.js
+++ level 2/JadenCase 문자열 만들기.js
@@ -1,6 +1,15 @@
-// LV2_연습문제: JadenCase 문자열 만들기 - 20220703
+// LV2_연습문제: JadenCase 문자열 만들기 - 20220703  /20230127
 
 // 최종 코드
 function solution(s) {
   return s.toLowerCase().replace(/\b\w/g, (a) => a.toUpperCase());
+}
+
+// 최종 코드2
+function solution2(s) {
+  return s
+    .toLowerCase()
+    .split(" ")
+    .map((a) => a.replace(a[0], a[0]?.toUpperCase()))
+    .join(" ");
 }
