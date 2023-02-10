a7778c43c1d0d7597570de9ca01de30d20a83c25
KimGaeun <greencloud11@hanyang.ac.kr>
2023-02-11 00:26:58 +0900

    solved 20230211

--- level 2/올바른 괄호.js
+++ level 2/올바른 괄호.js
@@ -1,4 +1,4 @@
-// LV2_스택/큐: 올바른 괄호 - 20220625
+// LV2_스택/큐: 올바른 괄호 - 20220625 / 20230211
 
 // 최종 코드
 function solution(s) {
@@ -10,13 +10,24 @@ function solution(s) {
   return num === 0 ? true : false;
 }
 
-// 정확성 테스트는 통과했지만 효율성 테스트에서 실패한 코드들
 function solution2(s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (stack.at(-1) + s[i] === "()") stack.pop();
+    else stack.push(s[i]);
+  }
+
+  return stack[0] ? false : true;
+}
+
+// 정확성 테스트는 통과했지만 효율성 테스트에서 실패한 코드들
+function solution3(s) {
   while (s.includes("()")) s = s.split("()").join("");
   return s === "" ? true : false;
 }
 
-function solution3(s) {
+function solution4(s) {
   while (s.includes("()")) {
     if (s[0] === "(") s = s.substring(1, s.length).replace(")", "");
     else return false;
@@ -24,7 +35,7 @@ function solution3(s) {
   return s === "" ? true : false;
 }
 
-function solution4(s) {
+function solution5(s) {
   let num = 0;
   let num2 = 0;
   while (num2 < s.length) {
