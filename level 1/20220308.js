273368ef0280ad7c7798edf245f39ee7469ac02e
KimGaeun <greencloud11@hanyang.ac.kr>
2023-02-17 18:27:49 +0900

    solved 20230217

--- level 1/20220308.js
+++ level 1/20220308.js
@@ -23,8 +23,30 @@ function solution(answers) {
   // ((e) => e != null)에서 !==가 아닌 !=를 사용해야 함
 }
 
+function solution2(answers) {
+  const arr = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
+  const numArr = [0, 0, 0];
+  const answer = [];
+
+  answers.forEach((a, i) => {
+    arr.forEach((b, j) => {
+      if (a === b[i % b.length]) numArr[j]++;
+    });
+  });
+
+  numArr.forEach((a, i) => {
+    if (a === Math.max(...numArr)) answer.push(i + 1);
+  });
+
+  return answer;
+}
+
 // 다른 사람 풀이
-function solution(answers) {
+function solution3(answers) {
   var answer = [];
   var a1 = [1, 2, 3, 4, 5];
   var a2 = [2, 1, 2, 3, 2, 4, 2, 5];
