0b3975a06b6c1039dfff12e287207d618ad662c7
KimGaeun <greencloud11@hanyang.ac.kr>
2023-02-25 20:54:18 +0900

    solved 20230225

--- level 2/숫자의 표현.js
+++ level 2/숫자의 표현.js
@@ -5,8 +5,7 @@ function solution(n) {
   let answer = 0;
 
   for (let i = 1; i <= n; i++) {
-    if (n % i === 0 && (n % 2 || (n / i) % 2)) answer++;
+    if (n % i === 0 && (n / i) % 2) answer++;
   }
-
   return answer;
 }
