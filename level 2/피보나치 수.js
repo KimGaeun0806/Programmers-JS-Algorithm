89c10410bea930c773e75e074bd483652a443f87
KimGaeun <greencloud11@hanyang.ac.kr>
2023-03-03 19:07:45 +0900

    solved 20230303

--- level 2/피보나치 수.js
+++ level 2/피보나치 수.js
@@ -1,4 +1,4 @@
-// LV2_연습문제: 피보나치 수 - 20221123
+// LV2_연습문제: 피보나치 수 - 20221123 / 20230303
 
 // 최종 코드
 function solution(n) {
@@ -9,4 +9,18 @@ function solution(n) {
   }
 
   return stack.at(-1);
+}
+
+// 최종 코드2
+function solution2(n) {
+  let prev = 1;
+  let prevprev = 1;
+
+  for (let i = 3; i <= n; i++) {
+    const newPrev = prev;
+    prev = (prev + prevprev) % 1234567;
+    prevprev = newPrev % 1234567;
+  }
+
+  return prev;
 }
