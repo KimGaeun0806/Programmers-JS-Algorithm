e4c09813a9f85aa69920976407ad32f22277a848
KimGaeun <greencloud11@hanyang.ac.kr>
2023-03-15 22:01:44 +0900

    solved 20230314

--- level 2/짝지어 제거하기.js
+++ level 2/짝지어 제거하기.js
@@ -1,8 +1,20 @@
-// LV2_2017 팁스타운: 짝지어 제거하기 - 20220820
+// LV2_2017 팁스타운: 짝지어 제거하기 - 20220820 / 20230315
 
 // 최종 코드
 function solution(s) {
   let stack = [""];
   for (let x of s) stack[stack.length - 1] === x ? stack.pop() : stack.push(x);
   return stack.length === 1 ? 1 : 0;
+}
+
+// 최종 코드2
+function solution2(s) {
+  const stack = [];
+
+  for (let x of s) {
+    if (stack.at(-1) === x) stack.pop();
+    else stack.push(x);
+  }
+
+  return stack.length ? 0 : 1;
 }
