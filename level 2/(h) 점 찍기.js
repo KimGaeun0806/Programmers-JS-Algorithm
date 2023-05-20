23962371045b9dbede134202c6a7b8ccb6bb4de6
KimGaeun <greencloud11@hanyang.ac.kr>
2023-05-20 23:10:04 +0900

    solved 20230520

---
+++ level 2/(h) 점 찍기.js
@@ -0,0 +1,34 @@
+// LV2_연습문제: 점 찍기
+
+// 20230520
+// 힌트 보고 푼 풀이
+function solution(k, d) {
+  let answer = 0;
+
+  for (let i = 0; i <= d; i += k) {
+    let max = Math.floor(Math.sqrt(d ** 2 - i ** 2));
+    answer += Math.floor(max / k) + 1;
+  }
+
+  return answer;
+
+  // 코드는 빠른 시간 내에 작성할 수 있었으나 시간초과로 고민하는 시간이 길었음
+  // for문 갯수를 줄이지 못해서 시간초과
+}
+
+// 시간 초과 났던 풀이
+function solution(k, d) {
+  const distance = (x, y) => Math.sqrt(x ** 2 + y ** 2);
+
+  let answer = 0;
+
+  for (let i = 0; i <= d; i += k) {
+    for (let j = 0; j <= d; j += k) {
+      if (distance(i, j) <= d) {
+        answer++;
+      }
+    }
+  }
+
+  return answer;
+}
